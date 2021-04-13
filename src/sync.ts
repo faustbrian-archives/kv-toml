a6811972a7fbf2c761ffd20b785c2aec263601f7
Brian Faust <hello@basecode.sh>
2021-04-13 17:02:49 +0300

    chore: integrate `@konceiver/tease`

--- src/sync.ts
+++ src/sync.ts
@@ -1,22 +1,28 @@
-// tslint:disable: no-unsafe-any
 import { JsonMap, parse, stringify } from "@iarna/toml";
 import { StoreSync as AbstractStore } from "@konceiver/kv-file";
-import { readFileSync, writeFileSync } from "fs-extra";
+import { ensureFileSync, readFileSync, writeFileSync } from "fs-extra";
 
 export class StoreSync<K, T> extends AbstractStore<K, T> {
 	public static new<K, T>(uri: string): StoreSync<K, T> {
 		return new StoreSync<K, T>(new Map<K, T>(), uri);
 	}
 
-	// @ts-ignore
-	protected dump(rows: Record<K, T>): void {
-		writeFileSync(this.uri, stringify((rows as unknown) as JsonMap));
+	protected dump(): void {
+		writeFileSync(this.uri, stringify((this.store as unknown) as JsonMap));
 	}
 
 	protected load(): void {
-		for (const [key, value] of Object.entries(parse(readFileSync(this.uri, "utf8")))) {
-			// @ts-ignore
-			this.put(key, value);
+		ensureFileSync(this.uri);
+
+		try {
+			for (const [key, value] of Object.entries(
+				parse(readFileSync(this.uri, "utf8"))
+			)) {
+				// @ts-ignore
+				this.put(key, value);
+			}
+		} catch {
+			//
 		}
 	}
 }
